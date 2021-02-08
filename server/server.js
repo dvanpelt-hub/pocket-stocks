51eaf6202c754f53124530d039952560665540c3
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-08 17:48:26 +0000

    Updated stock

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/stocks", (req, res) => {
-  console.log("Get all restaurants");
+  console.log("Get all stocks!");
 })
 
 // Use env variable, else use PORT 3002 //
